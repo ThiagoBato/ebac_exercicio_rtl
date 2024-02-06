9721c0ad546bf4bfd6bf72f892ff5efc47226798
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2024-02-06 11:29:37 -0300

    Adding the module 33 exercise

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # Requisitos
+
 ## Instalar as dependências
+
 `npm install` ou `yarn install`
 
 ## Para executar os testes
+
 `npm run test` ou `yarn test`
 
 ## Para executar a aplicação em localhost
+
 `npm start` ou `yarn start`
 
 # Para o exercício
+
 O arquivo onde o teste deve ser escrito está em src/components/PostComments/PostComments.test.tsx
 
 ## Resolução
+
 Neste repositório existe uma branch chamada **resolucao** com o exercício resolvido, é importante que a utilize como consulta caso tenha dificuldades ou até mesmo para comparar com a abordagem que você adotou, **não faça simplesmente um copiar/colar** - confio em você ;)
+
+<!--
+Neste exercício você deve praticar o uso do react testing library.
+1) Façar um fork do repositório que está disponível no link https://github.com/ogiansouza/ebac_exercicio_rtl;
+2) Crie um código para testar a inserção de dois comentários;
+3) Utilize o atributo data-testid;
+4) Atualize o repositório com o teste criado;
+5) Envie o link para a EBAC.
+-->
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('PostComment Test', () => {
+  it('Should render correctly', () => {
+    render(<PostComment />);
+    expect(screen.getByTestId('comment-button')).toBeInTheDocument();
+  });
+
+  it('Should add 2 comments to the list', () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+      target: {
+        value: 'First comment added through tests',
+      },
     });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    fireEvent.change(screen.getByTestId('comment-textarea'), {
+      target: {
+        value: 'Second comment added through tests',
+      },
+    });
+    fireEvent.click(screen.getByTestId('comment-button'));
+
+    expect(screen.getAllByTestId('post-comment-element')).toHaveLength(2);
+  });
 });
